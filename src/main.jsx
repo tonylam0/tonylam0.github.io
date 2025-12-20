dc32b6ce6ca4b8bd44987f56c0df4779b287419c
Tonycs <lam.tony540@gmail.com>
2025-12-20 07:08:19 -0500

    Added about page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Homepage from './pages/Homepage/Homepage.jsx'
+import About from './pages/About/About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import styles from './About.module.css'
+
+function About() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default About
