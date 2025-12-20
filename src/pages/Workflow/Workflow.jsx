b3514320b13fe850a9666c834eb072f1a4bcf593
Tonycs <lam.tony540@gmail.com>
2025-12-20 13:18:37 -0500

    Fixed mobile workflow scrolling

--- src/pages/Workflow/Workflow.jsx
+++ src/pages/Workflow/Workflow.jsx
@@ -7,19 +7,19 @@ const Workflow = () => {
         <div className={styles.content}>
           <h1>Workflow</h1>
           <p>List of how I go upon work and the things I use along the way.</p>
-          <h2>Coding</h2>
+          <h2 className={styles.heading}>Coding</h2>
           <p>Still writing...</p>
-          <h2>Taking Notes for School</h2>
+          <h2 className={styles.heading}>Taking Notes for School</h2>
           <p>Still writing...</p>
-          <h2>Reading</h2>
+          <h2 className={styles.heading}>Reading</h2>
           <p>Still writing...</p>
-          <h2>To-do list</h2>
+          <h2 className={styles.heading}>To-do list</h2>
           <p>Still writing...</p>
-          <h2>Tracking</h2>
+          <h2 className={styles.heading}>Tracking</h2>
           <p>Still writing...</p>
-          <h2>Reducing Distractions</h2>
+          <h2 className={styles.heading}>Reducing Distractions</h2>
           <p>Still writing...</p>
-          <h2>Contemplation</h2>
+          <h2 className={styles.heading}>Contemplation</h2>
           <p>Still writing...</p>
         </div>
       </div>
