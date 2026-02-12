0c374297ee9ff44bb15756267343920242334755
tonylam0 <tonylam0@users.noreply.github.com>
2026-02-12 10:47:16 +0000

    deploy: 5625c832436dacce59091c4b3d0e838166e1a8fd

---
+++ assets/gold-standard-mnL-ciDF.js
@@ -0,0 +1,28 @@
+import{j as e,M as t,r as n}from"./index-C4uMIv1l.js";import{P as o}from"./Post-DlJY0let.js";const i=`# Escaping the "Gold Standard" of Computer Science
+
+### The paradox
+> "Sometimes it's weird to consider that we're all effectively competing to hit the right keys on our keyboards in the right order and that if we do it for long enough, we can change the world."
+
+It's hard to say what initially draws so many people to computer science. Perhaps it's easy to narrow it down to the common love of math and/or problem-solving, or the relatively low amount of requirements needed to start coding: a computer and the willingness to learn. But, I'd argue that in conjunction with these two, the real draw of computer science lies in its *versatility*.
+
+Personally, I've always been the type of person who wants to try everything. Whether that be running, weight-lifting, reading, drawing, making YouTube videos, the list goes on. So, I wanted to integrate that same well-roundedness in my career choice. Coding was the obvious answer. With time, any coder can create **anything** they want: a productivity app, a robot, a children's game, etc.
+
+Yet, despite this, people treat computer science as a one-lane highway. If you are pursuing computer science, you *must*:
+
+* Grind LeetCode questions
+* Apply apply apply to endless internships
+* Attend as many career fairs as possible to meet potential hirers
+* Create a personal portfolio website
+* Spend your free time working on side projects for your resume
+
+In the field, with one of its biggest strengths being the potential to go down so many *different* routes, computer science students are treated as crazy if they do not follow the **Gold Standard Guide to Become a Software Engineer (SWE) at a Big Tech Company**, which stifles the very versatility that makes the discipline special. Unfortunately, this robotic movement of resume padding has turned many curious and inspired minds into just *another computer science student*.
+
+Now, with this said, having the aspiration to be a software engineer is not a bad thing in and of itself. They are, in their own right, very much capable and bright thinkers, but the problem arises when those who *aren't* aspiring software engineers get swept up in the hypnotic rhythm of the SWE community.
+
+### What now?
+> "Sometimes the only thing that'll make a person move is being left behind."
+
+The most crucial step for change to occur in the currently close-minded landscape is for more people to take that tangent step and to **actually be successful**. Yes, being ambitious and having big goals is great, but only if you allow those ideas to blossom through your tangible work.
+
+When one person has the *courage* to stand up *against the current* and *thrive*, others find it hard to ignore them. Normalities that we see now as commonplace in society, like equality and technology, were paved by those who didn't follow normal ideas. Only through *overwhelming willpower* and *the ability to say no* to the standard convention were society's preconceptions able to alter. Computer science, nor any other field, will ever be entirely filled with innovators, but it only takes one fearless individual for an entire generation to be moved forward.
+`,s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{date:"2/11/2026",children:e.jsx(t,{rehypePlugins:[[n,{target:"_blank",rel:["noopener","noreferrer"]}]],children:i})})});export{s as default};
